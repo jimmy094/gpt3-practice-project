ea21469bfdf2183bfa14d2c46b67883b9ccd1300
jimmyalvarez <jimmyalvarezjr@gmail.com>
2021-11-30 14:44:11 -0500

    header

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,8 +3,28 @@ import './Brand.css'
 
 const Brand = () => {
     return (
-        <div>
+        <div className='gpt3__brand section__padding'>
             
+            <div>
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/google.png?raw=true" alt="google" />
+            </div>
+
+            <div>
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/slack.png?raw=true" alt="slack" />
+            </div>
+
+            <div>
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/atlassian.png?raw=true" alt="atlassian" />
+            </div>
+
+            <div>
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/dropbox.png?raw=true" alt="dropbox" />
+            </div>
+
+            <div>
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/shopify.png?raw=true" alt="shopify" />
+            </div>
+
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './Header.css'
+//src/assets /people.png
+
+
+
 
 const Header = () => {
     return (
-        <div>
-           Header 
+        <div className='gpt3__header section__padding' id="home">
+           <div className="gpt3__header-content">
+
+               <h1 className='gradient__text'>Lets build something great with GPT-3 OpenAI</h1>
+               <p className='body-text'>GPT-3 AI body text p tag sign up and get started now!</p>
+
+                <div className="gpt3__header-content__input">
+                    <input type='email' placeholder='Your email' />
+                    <button className='button'>Get started!</button>
+                </div>
+
+                <div className="gpt3__header-content__people">
+                    <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/people.png?raw=truealt='people'"/>
+                    <p>1600 people requested access to visit in the last 24 hours</p>
+                </div>
+
+           </div>
+
+
+            <div className="gpt3__header-image">
+                <img src="https://github.com/adrianhajdin/project_modern_ui_ux_gpt3/blob/main/src/assets/ai.png?raw=true" alt='ai'/>
+            </div>
+
         </div>
     )
 }
